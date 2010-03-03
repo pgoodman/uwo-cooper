c214b157d61c8a8fa912c0e3d0167dd015c8e60a
peter.goodman <peter.goodman@c307de66-1bdf-11df-a447-cf726199f266>
2010-03-03 05:35:40 +0000

    forgot to add a file.

---
+++ trunk/conf.h
@@ -0,0 +1,14 @@
+/*
+ * conf.h
+ *
+ *  Created on: Mar 2, 2010
+ *      Author: petergoodman
+ *     Version: $Id$
+ */
+
+#ifndef CONF_H_
+#define CONF_H_
+
+#define COORDINATOR_USER_NAME "coord"
+
+#endif /* CONF_H_ */
