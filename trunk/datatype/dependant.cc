e757b52b49776e67ca13d8cbf68acff00458b2d6
nick.perreault <nick.perreault@c307de66-1bdf-11df-a447-cf726199f266>
2010-03-03 18:05:09 +0000

    #include "dependant.h"
    Dependant::Dependant()
    {
    }
    
    Dependant::Dependant(string name, time_t bday)
    {
    	fullName = name;
    	timeOf21stBday = bday;
    }
    
    bool Dependant::isUnderAge(time_t currentTime)
    {
    	return (timeOf21stBday < currentTime);
    }
    
    Dependant::~Dependant()
    {
    	delete fullName;
    	delete timeOf21stBday;
    }

--- trunk/datatype/dependant.cc
+++ trunk/datatype/dependant.cc
@@ -3,3 +3,20 @@
 Dependant::Dependant()
 {
 }
+
+Dependant::Dependant(string name, time_t bday)
+{
+	fullName = name;
+	timeOf21stBday = bday;
+}
+
+bool Dependant::isUnderAge(time_t currentTime)
+{
+	return (timeOf21stBday < currentTime);
+}
+
+Dependant::~Dependant()
+{
+	delete fullName;
+	delete timeOf21stBday;
+}
