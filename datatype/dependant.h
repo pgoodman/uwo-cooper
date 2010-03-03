4bd7e55d82b5f75910ebedbcd6ac2164053ad630
nick.perreault <nick.perreault@c307de66-1bdf-11df-a447-cf726199f266>
2010-03-03 17:48:16 +0000

    #include "dependant.h"
    Dependant::Dependant()
    {
    }
    
    Dependant::Dependant(string name, time_t bday)
    {
    	fullName = name;
    	timeOf21stBday = bday;
    }

--- datatype/dependant.h
+++ datatype/dependant.h
@@ -1,10 +1,28 @@
 #ifndef DEPENDANT_H
 #define DEPENDANT_H
 
+#include <string>
+
+using namespace std;
+
+
 class Dependant
 {
 public:
-    Dependant();
+	//Constructors
+	Dependant();
+	Dependant(string name, time_t bday);
+
+	//methods
+	bool isUnderAge();
+
+	//accessor methods
+	string getName(){return name;}
+	time_t getBday(){return timeOf21stBirthday;}
+
+private:
+	string name;
+	time_t timeOf21stBirthday;
 };
 
 #endif // DEPENDANT_H
