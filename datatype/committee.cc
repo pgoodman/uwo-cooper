f1990e6cc7ee8bfa1ffaa96af45f7531d9d8530e
stephanb89 <stephanb89@c307de66-1bdf-11df-a447-cf726199f266>
2010-03-04 06:07:44 +0000

    Partially done committee class, the only issue is that its giving me a weird error about Member either not being a data type in some cases or not being called in other cases which is weird. Anyways, heres what I have done commented out. I can finish more tomorrow in class :D

--- datatype/committee.cc
+++ datatype/committee.cc
@@ -1,5 +1,66 @@
 #include "committee.h"
+#include "permission.h"
+#include "member.h"
+#include <string>
+#include <list>
 
-Committee::Committee()
-{
+/*
+string name;
+//name std::string[1]
+bool can_delete;
+Member chair;
+Member secretary;
+Permission chair_perms;
+Permission member_perms;
+list<Member> memberList;
+*/
+
+Committee::Committee() {
 }
+
+/*
+Committee::Committee(string committeeName, Member committeeChair, Member committeeSecretary)
+{
+        name = committeeName;
+        chair = committeeChair;
+        secretary = committeeSecretary;
+        memberList = new list<Member>;
+        // Initiate chair perms and member perms
+}
+*/
+
+int getId(void) {
+    // UNSURE WHAT THIS IS RETURNING EXACTLY
+    return 0;
+}
+
+static Committee *load(int id) {
+    (void) id; return 0;
+}
+
+/*
+Permission Committee::getPermissions(Member member) {
+    if (member.getID() == chair.getID()) {
+        return chair_perms;
+    }
+    else
+        return member_perms;
+}
+
+void Committee::addMember(Member member) {
+    //Add a member to the committees member list.
+    // NOTE: USING LIST AS EXAMPLE, DONT KNOW HOW WERE GOING TO STORE THIS!!!
+
+    memberList.insert(member);
+}
+
+void Committee::removeMember(Member member) {
+    // Remove a member from a committees member list.
+    //NOTE: USING LIST AS EXAMPLE, DONT KNOW HOW WERE GOING TO STORE THIS!!!
+
+    memberList.remove(member);
+}
+
+//void Committee::addTask(Task task);
+//void Committee::removeTask(Task task);
+*/
