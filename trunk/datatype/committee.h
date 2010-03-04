a812c279e3e109a4b21f835346a9af8ea44e9bdf
stephanb89 <stephanb89@c307de66-1bdf-11df-a447-cf726199f266>
2010-03-04 06:07:44 +0000

    Partially done committee class, the only issue is that its giving me a weird error about Member either not being a data type in some cases or not being called in other cases which is weird. Anyways, heres what I have done commented out. I can finish more tomorrow in class :D

--- trunk/datatype/committee.h
+++ trunk/datatype/committee.h
@@ -1,12 +1,29 @@
 #ifndef COMMITTEE_H
 #define COMMITTEE_H
 
+#include "permission.h"
+
 class Committee
 {
+
 public:
     Committee();
     int getId(void) { return 0; }
     static Committee *load(int id) { (void) id; return 0; }
+   /* Permission getPermissions(Member member);
+    void addMember(Member member);
+    void removeMember(Member member);
+   // void addTask(Task task);
+   // void removeTask(Task task);
+
+private:
+    //name: std::string[1]
+    bool can_delete;
+    Member chair;
+    Member secretary;
+    Permission chair_perms;
+    Permission member_perms;
+    */
 };
 
 #endif // COMMITTEE_H
