5bbfd0c7126f7a9304a434d83e304c9d51a04120
nick.perreault <nick.perreault@c307de66-1bdf-11df-a447-cf726199f266>
2010-03-05 18:29:55 +0000

    added in unit table

--- datatype/unit.cc
+++ datatype/unit.cc
@@ -1,5 +1,5 @@
 #include "unit.h"
-
+#include "cooperdb.h"
 Unit::Unit()
 {
 }
@@ -9,6 +9,31 @@ Unit::Unit(int roomNumber, QString address, int bedrooms)
 	number = roomNumber;
 	streetAddress = address;
 	numBedrooms = bedrooms;
+}
+
+Unit::save(void)
+{
+    QSqlQuery q;
+    q.prepare(
+            "UPDATE unit set number=?,streetAddress=?,numBedrooms=?,id=?");
+    q << number << streetAddress << numBedrooms << id;
+
+    if(!q.exec()) {
+        CooperDB::queryError("Unable to Save Unit", q);
+    }
+}
+
+void Unit::create(int rNumber, QString address, int nRooms, int id) {
+    QSqlQuery q;
+    q.prepare(
+        "INSERT INTO unit (number,streetAddress,numBedrooms,id)"
+        "VALUES (?,?,?,?)"
+    );
+    q << rNumber << address << nRooms << id;
+
+    if(!q.exec()) {
+        CooperDB::queryError("Unable to Add Committee", q);
+    }
 }
 
 Unit::~Unit() {
